3d5187d522b7d5356a4fefaac9ca9493b4e1a255
Evgeny <abaevevgenchik@gmail.com>
2021-01-02 22:53:47 +0300

    b

--- BookProject/settings.py
+++ BookProject/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
-)
+)'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
